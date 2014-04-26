5d51b7750d684b21c04c868b3b84b0204cedeed7
Len Kutchma <lkutchma@gmail.com>
2014-04-25 23:45:17 -0500

    removed custom footer from Landing Page

--- page_landing.php
+++ page_landing.php
@@ -42,6 +42,7 @@ remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+remove_action( 'genesis_footer', 'marcus_footer' );
 
 //* Run the Genesis loop
 genesis();
